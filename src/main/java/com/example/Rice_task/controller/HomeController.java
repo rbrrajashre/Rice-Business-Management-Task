3897c410e04df0bbe9008111d9fb85e8efd7c90e
RB.Rajashre Rajesware <rbr.rajashre2004@gmail.com>
2025-09-27 19:43:12 +0530

    Create HomeController.java

---
+++ src/main/java/com/example/Rice_task/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.example.Rice_task.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "index";  // maps to index.html template
+    }
+}
